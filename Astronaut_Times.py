703a426b7410bbfbebbc7709b88899dbc17ab467
Vony <24597915+Vony7@users.noreply.github.com>
2021-09-10 00:15:49 -0600

    Update Astronaut_Times.py

--- Astronaut_Times.py
+++ Astronaut_Times.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-"""
 from dateutil import parser
 import datetime
 import matplotlib.colors as colors
@@ -30,6 +27,7 @@ class Astronaut:
         self.age = (datetime.datetime.utcnow()+datetime.timedelta(hours=8)-self.DOB).days/365.2425
         self.EVAs = []  
         self.EVA_time =[]
+        self.total_eva_time = 0
     def getDetails(self):
         print("Name: ", self.name)
         print("Gender: ",self.gender)
@@ -46,8 +44,11 @@ class Astronaut:
         self.mission_time.append(mission.duration)
         self.total_time = sum(self.mission_time)
         
-    def addEVA(self, EVA):
+    def addEVA(self,EVA):
         self.EVAs.append(EVA)
+        self.num_of_evas = len(self.EVAs)
+        self.EVA_time.append(EVA.duration)
+        self.total_eva_time = sum(self.EVA_time)
         
 class Mission:
     def __init__(self,name,mid,start,end,crews):
@@ -67,12 +68,14 @@ class Mission:
         self.number_of_crew = len(self.crews)
         
 class EVA:
-    def __init__(self, eva_id,start_time, end_time, crew, mission):
+    def __init__(self, eva_id,start_time, end_time, crews, mission):
         self.eva_id = eva_id
+        self.crews = crews
         self.start_time = parser.parse(start_time)
         self.end_time = parser.parse(end_time)
-        self.duration = self.end_time-self.start_time
-        
+        self.duration = (self.end_time-self.start_time).total_seconds()/3600
+        for crew in crews:
+            crew.addEVA(self)
 
 #%% Add Astronauts
 # Name, uid, gender, date of birth
@@ -102,7 +105,8 @@ sz12= Mission("神舟十二号",'sz12','2021/06/17 9:22:00',0,[nhs,lbm,thb])
 
 missions = [sz5,sz6,sz7,sz9,sz10,sz11,sz12]
 # Add EVAs
-sz7eva1 = EVA("cz7eva1",'2008/9/27 16:35','2008/9/27 17:01',[zzg,lbm],sz7)
+#EID,start, end,[crew1,crew2],mission
+sz7eva1 = EVA("sz7eva1",'2008/9/27 16:35','2008/9/27 17:01',[zzg,lbm],sz7)
 sz12eva1 = EVA("sz12eva1",'2021/7/5 8:11','2021/7/5 14:57',[lbm,thb],sz12)
 sz12eva2 = EVA("sz12eva2",'2021/8/21 8:38','2021/8/21 14:33',[nhs,lbm],sz12)
 EVAs = [sz7eva1,sz12eva1,sz12eva2]
@@ -113,6 +117,62 @@ fprop_title = fm.FontProperties(fname='font/ZhiMangXing-Regular.ttf')
 fprop = fm.FontProperties(fname='font/NotoSansSC-Regular.otf')
 y_names,x_vals = zip(*[(i.name,float(i.num_of_missions)) for i in sorted_astro])
 y_pos = np.arange(len(sorted_astro))
+#%% EVA Time
+eva_total = [hty.total_eva_time for hty in astro]
+c = np.zeros((len(EVAs),len(astro)))
+fig1,ax = plt.subplots(figsize=(16,12),dpi=300)
+astro_names = [hty.name for hty in astro]
+legend_names =[eva.eva_id for eva in EVAs]
+bottom = 0
+for eva in EVAs:
+    idx_eva = EVAs.index(eva)
+    for crew in eva.crews :
+        idx_crew = astro.index(crew)
+        c[idx_eva,idx_crew] = eva.duration
+    ptt = plt.bar(astro_names,c[idx_eva],bottom=bottom)
+    #axx.bar_label(ptt,label_type='center',fmt='%.2f')
+    bottom +=c[idx_eva]
+I=plt.legend(legend_names,prop=fprop,loc='upper center',facecolor='black',ncol=len(astro),frameon=False)
+plt.plot(astro_names,eva_total,'.r')
+for text in I.get_texts():
+    text.set_color('white')
+# add data labels
+for rect in ax.patches:
+    height = rect.get_height()
+    width = rect.get_width()
+    x = rect.get_x()
+    y = rect.get_y()
+    label_text = f'{height:.2f}'
+    label_x = x+width/2
+    label_y = y+height/2
+    if height>0:
+        ax.text(label_x,label_y,label_text, color='white',ha = 'center', va = 'center',fontsize = 8)
+#make labels
+for i in range(len(astro)):
+    datastr = "{:.2f}".format(eva_total[i])
+    plt.annotate(datastr,xy=(astro_names[i],eva_total[i]),ha='center',va='bottom',color='white')
+plt.xlabel("航天员", fontproperties=fprop,fontsize=20,color='white')
+ymax = np.amax(eva_total)
+ax.set_xticklabels(astro_names,fontproperties=fprop,fontsize=16,color='white')
+ax.set_yticks(np.arange(0,ymax+10,step=1))
+ax.set_yticklabels(np.arange(0,ymax+10,step=1),fontproperties=fprop,fontsize=16,color='white')
+#data labels
+plt.ylabel("出舱时间（小时）", fontproperties=fprop,fontsize=20,color='white')
+plt.ylim(0,np.amax(eva_total)+2)
+plt.title("中国航天员出舱时间统计",fontproperties=fprop_title,fontsize=40,color='white')
+now = datetime.datetime.utcnow()+datetime.timedelta(hours=8)
+ax.yaxis.set_minor_locator(tck.AutoMinorLocator())
+ax.text(.4, 0.95,"截至北京时间："+ now.strftime("%Y/%m/%d %H:%M:%S.%f"), fontproperties=fprop,color="gray",transform=ax.transAxes,va='center')
+ax.text(.45, 0.92,"绘制：@Vony7", fontproperties=fprop,color="gray", transform=ax.transAxes)
+ax.set_facecolor("black")
+plt.rcParams['savefig.facecolor']='black'
+ax.spines['bottom'].set_color('white')
+ax.spines['top'].set_color('white') 
+ax.spines['right'].set_color('white')
+ax.spines['left'].set_color('white')
+ax.tick_params(axis='x', colors='white')
+ax.tick_params(axis='y', which='both',colors='white')
+plt.savefig('eva-time-stacked.png')
 
 #%% plot different missions
 # plot bars in stack manner
@@ -131,7 +191,7 @@ for sz in missions:
     #axx.bar_label(ptt,label_type='center',fmt='%.2f')
     bottom +=c[idx_sz]
 I=plt.legend(legend_names,prop=fprop,loc='upper center',facecolor='black',ncol=len(astro),frameon=False)
-plt.plot(astro_names,astro_total,'.k')
+plt.plot(astro_names,astro_total,'.r')
 for text in I.get_texts():
     text.set_color('white')
 # add data labels
@@ -172,83 +232,4 @@ axx.spines['left'].set_color('white')
 axx.tick_params(axis='x', colors='white')
 axx.tick_params(axis='y', which='both',colors='white')
 plt.savefig('mission-time-stacked.png')
-#%% plot person by person
 
-""" #%% Plot Number of Missions
-fig, ax = plt.subplots(figsize=(8,6),dpi=300)
-ax.barh(y_pos, x_vals)
-ax.set_yticks(y_pos)
-ax.set_yticklabels(y_names,fontproperties=fprop,fontsize=16,rotation=0)
-ax.bar_label(ax.containers[0],fmt='%.0f')
-rlim = max(x_vals)+1
-xtick = np.arange(rlim)
-ax.set_xticks(xtick)
-ax.set_xlim(right=rlim)  # adjust xlim to fit labels
-ax.set_xlabel('任务次数（次）',fontproperties=fprop,fontsize=16,rotation=0)
-now = datetime.datetime.utcnow()+datetime.timedelta(hours=8)
-ax.text(.64, 0.05,"截至北京时间："+ now.strftime("%Y/%m/%d %H:%M:%S"), fontproperties=fprop,transform=ax.transAxes,va='center')
-#plt.tight_layout()
-plt.savefig("number_of_missions.png")
-
-#%% Plot Mission Time
-# color map
-#colours = {'FIRST':'#FC6238','SECOND':'#FFD872','THIRD':'#F2D4CC','FORTH':'#E77577','FIFTH':'#6C88C4'}
-colours = {'FIRST':'#173F5F','SECOND':'#20639B','THIRD':'#3CAEA3','FORTH':'#F6D55C','FIFTH':'#ED553B'}
-
-def next_key(dict, key):
-    keys = iter(dict)
-    key in keys
-    return next(keys, False)
-
-sorted1 = sorted(astro,key = operator.attrgetter("total_time"))
-y_names,x_vals = zip(*[(i.name,float(i.total_time)) for i in sorted1])
-
-fig2,ax2 = plt.subplots(figsize=(8,6), dpi=300)
-for k in range(len(sorted1)):
-    bt = 0
-    hbar = ax2.barh(k,sorted1[k].missions[0].duration,color=colours['FIRST'])
-    key = next_key(colours,'FIRST')
-    h = 1
-    while h<len(sorted1[k].missions):
-        bt+=sorted1[k].missions[h-1].duration
-        hbar = ax2.barh(k,sorted1[k].missions[h].duration,left=bt,color=colours[key])
-        key = next_key(colours,key)
-        h+=1
-    ax2.bar_label(hbar,fmt='%.2f')    
-ax2.set_yticks(y_pos)
-ax2.set_yticklabels(y_names,fontproperties=fprop,fontsize=16,rotation=0)
-rlim = max(x_vals)+8
-ax2.set_xlim(right=rlim)  # adjust xlim to fit labels
-ax2.text(.62, 0.1,"截至北京时间："+ now.strftime("%Y/%m/%d %H:%M:%S"), fontproperties=fprop,transform=ax.transAxes,va='center')
-ax2.text(.62,0.05,"制图：@Vony7", fontproperties=fprop,transform=ax.transAxes,va='center')
-ax2.set_xlabel('任务时间（天）',fontproperties=fprop,fontsize=16,rotation=0)
-#plt.tight_layout()     
-plt.savefig("total mission time.png") """
-
-#%% Plot different color for each mission
-# plot bars in stack manner
-""" fig3,ax3 = plt.subplots(figsize=(8,6),dpi=300)
-for k in range(len(sorted1)):
-    bt = 0
-    hbar = ax3.barh(k,sorted1[k].missions[0].duration,color=colours['FIRST'])
-    key = next_key(colours,'FIRST')
-    h = 1
-    while h<len(sorted1[k].missions):
-        bt+=sorted1[k].missions[h-1].duration
-        hbar = ax3.barh(k,sorted1[k].missions[h].duration,left=bt)
-        key = next_key(colours,key)
-        h+=1
-    ax3.bar_label(hbar,fmt='%.2f')    
-ax3.set_yticks(y_pos)
-ax3.set_yticklabels(y_names,fontproperties=fprop,fontsize=16,rotation=0)
-rlim = max(x_vals)+8
-ax3.set_xlim(right=rlim)  # adjust xlim to fit labels
-ax3.text(.62, 0.1,"截至北京时间："+ now.strftime("%Y/%m/%d %H:%M:%S"), fontproperties=fprop,transform=ax.transAxes,va='center')
-ax3.text(.62,0.05,"制图：@Vony7", fontproperties=fprop,transform=ax.transAxes,va='center')
-ax3.set_xlabel('任务时间（天）',fontproperties=fprop,fontsize=16,rotation=0)
-#plt.tight_layout()     
-#plt.legend(loc='lower right')
-plt.savefig("total mission time stacked all.png") """
-
-
-#%% Plot based on EVA time
