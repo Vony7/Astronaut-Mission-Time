e5a9a8857911ef0d8eb19964a21935bf055d30db
Vony <24597915+Vony7@users.noreply.github.com>
2021-11-13 21:43:03 -0700

    Add files via upload

---
+++ launch_global.py
@@ -0,0 +1,93 @@
+#global launch 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from pathlib import Path
+from dateutil import parser
+#from datetime import timedelta
+from datetime import datetime
+from pylab import *
+import pytz
+import matplotlib.font_manager as fm
+fprop_title = fm.FontProperties(fname='font/ZhiMangXing-Regular.ttf')
+fprop = fm.FontProperties(fname='font/NotoSansSC-Regular.otf')
+datatxt = 'launchglobal'
+token = open(datatxt + '.txt','r',encoding = 'utf8')
+linestoken=token.readlines()
+launch_time = []
+launch_country = []
+launch_results = []
+launch_rockets = []
+for x in linestoken:
+    if not x.startswith("#"):
+        from datetime import datetime
+        time_liftoff = datetime.strptime(x.split()[0],'%m/%d/%YT%H:%M')
+        launch_time.append(time_liftoff)
+        launch_country.append(x.split()[1])
+        launch_results.append(int(x.split()[2]))
+        launch_rockets.append(x.split()[3])
+token.close()
+
+# Process Data
+rockets = np.unique(launch_rockets)
+# Launch countries
+countries = np.unique(launch_country)
+xaxis_labels = ['中国','欧洲','印度','伊朗','日本','俄罗斯','韩国','美国']
+# Launch countries x time
+launch_total = np.zeros((len(launch_time),countries.size),dtype=int)
+color_country = ['red','green','#3989b9','#322825','#79a49e','#3c7ba6','#193852','blue','#c6ca74','#1a425b']
+launch_success = np.zeros(len(countries),dtype=int)
+launch_failure = np.zeros(len(countries),dtype=int)
+launch_overall = np.zeros(len(countries),dtype=int)
+print(launch_failure)
+for i in np.arange(0,len(launch_time)):
+    country = launch_country[i]
+    idx = [i for i,x in enumerate(countries) if x ==country]
+    if(launch_results[i]>0):
+        launch_success[idx]+=1
+    else:
+        launch_failure[idx]+=1
+    launch_total[i][idx]=1
+    launch_overall +=1
+    if(i>0):
+        launch_total[i]=launch_total[i-1]
+        launch_total[i][idx]=launch_total[i-1][idx]+1 
+# Step PLOT by Country
+fig,ax = plt.subplots(1,figsize=(12,8),dpi=200)
+for j in np.arange(0,countries.size):
+    x_value = launch_time
+    y_value = launch_total[:,j]
+    plt.step(x_value,y_value,'-',color = color_country[j],label=xaxis_labels[j])
+plt.legend(prop =fprop)
+ax.yaxis.set_major_locator(MultipleLocator(5))
+ax.yaxis.set_minor_locator(MultipleLocator(1))
+from datetime import datetime
+time_now = datetime.now(pytz.timezone('Asia/Shanghai')).strftime('%Y/%m/%d %H:%M:%S')
+ax.text(.3, 0.95,"截至北京时间："+ time_now, fontproperties=fprop,color="gray",transform=ax.transAxes,va='center')
+ax.text(.32, 0.90,"绘制：@Vony7", fontproperties=fprop,color="gray", transform=ax.transAxes)
+plt.title('2021年全球航天入轨发射统计',fontproperties = fprop_title, fontsize = 30)
+plt.xlabel('时间',fontproperties=fprop)
+plt.ylabel('发射次数',fontproperties=fprop)
+plt.ylim(ymin=0)
+plt.xlim(xmin=min(x_value),xmax=max(x_value))
+ax.yaxis.tick_right()
+ax.yaxis.set_label_position('right')
+plt.savefig('launch_global.png')
+
+#%% Bar By Rocket
+fig,ax = plt.subplots(1,figsize=(8,6),dpi=300)
+plt.bar(countries,launch_failure,color = '#e22030',label='失败')
+plt.bar(countries, launch_success,bottom = launch_failure, color = '#00aeac',label='成功')
+
+ax.set_xticklabels(xaxis_labels,fontproperties = fprop)
+from datetime import datetime
+time_now = datetime.now(pytz.timezone('Asia/Shanghai')).strftime('%Y/%m/%d %H:%M:%S')
+ax.text(.3, 0.85,"截至北京时间："+ time_now, fontproperties=fprop,color="gray",transform=ax.transAxes,va='center')
+ax.text(.42, 0.80,"绘制：@Vony7", fontproperties=fprop,color="gray", transform=ax.transAxes)
+ax.yaxis.set_major_locator(MultipleLocator(10))
+ax.yaxis.set_minor_locator(MultipleLocator(1))
+plt.ylabel('发射次数', fontproperties = fprop)
+plt.title('2021年全球航天入轨发射统计',fontproperties = fprop_title, fontsize = 30)
+plt.legend(loc='upper center', prop =fprop)
+plt.savefig('launch_2021_barplot.png')
