9c21057a53674834e3eea2445403211be392587c
Vony <j.dong@utah.edu>
2021-11-27 00:32:30 -0700

    launch by rockets

--- launch_global.py
+++ launch_global.py
@@ -37,7 +37,7 @@ rockets = np.unique(launch_vehicles)
 countries = np.unique(launch_country)
 # Launch Sites
 L_sites = np.unique(launch_sites)
-# Launch Vehicles
+# Launch vechicles
 L_vehicles = np.unique(launch_vehicles)
 c_dict = {'CHN':'中国','ESA':'欧空局','IND':'印度','IRN':'伊朗','JPN':'日本','RUS':'俄罗斯','SKO':'韩国','USA':'美国'}
 # Launch countries x time
@@ -47,9 +47,15 @@ launch_success = np.zeros(len(countries),dtype=int)
 launch_failure = np.zeros(len(countries),dtype=int)
 launch_overall = np.zeros(len(countries),dtype=int)
 launch_Bysites = np.zeros(len(L_sites),dtype=int)
+launch_Byvehicles = np.zeros(len(L_vehicles),dtype=int)
 for i in np.arange(0,len(launch_time)):
+    #launch sites
     site_idx = [id for id,x in enumerate(L_sites) if x ==launch_sites[i]]
     launch_Bysites[site_idx]+=1
+    #launch vehicles
+    lv_idx = [id for id,x in enumerate(L_vehicles) if x ==launch_vehicles[i]]
+    launch_Byvehicles[lv_idx]+=1
+    #launch country
     country = launch_country[i]
     idx = [i for i,x in enumerate(countries) if x ==country]
     if(launch_results[i]>0):
@@ -142,42 +148,12 @@ for site in L_sites:
     s_bar_color = cc_dict[s_country]
     site_colors.append(s_bar_color)
     #print(site,s_country,s_bar_color)
-fig2,ax2 = plt.subplots(1,figsize=(12,8),dpi=200)
 site_colors = np.array(site_colors)
-plt.bar(L_sites[sites_idx],launch_Bysites[sites_idx],color = site_colors[sites_idx])
 x_labels=[]
 for site in L_sites:
     x_labels.append(dict_sites[site])
 x_labels = np.array(x_labels)
-ax2.xaxis.set_ticks(np.arange(0,len(dict_sites)))
-ax2.xaxis.set_ticklabels(x_labels[sites_idx],fontproperties = fprop)
-ax2.yaxis.set_major_locator(MultipleLocator(5))
-ax2.yaxis.set_minor_locator(MultipleLocator(1))
 
-# Data Labels
-for rect in ax2.patches:
-    y_value = rect.get_height()
-    x_value = rect.get_x()+rect.get_width()/2
-    space = 0
-    va = 'bottom'
-    if y_value<0:
-        space*=-1
-        va='top'
-    label="{:.0f}".format(y_value)
-    ax2.annotate(
-        label,
-        (x_value,y_value),
-        xytext=(0,space),
-        textcoords = "offset points",
-        ha = 'center',
-        va = va
-    )  
-plt.title('2021年全球航天入轨各发射场统计',fontproperties = fprop_title, fontsize = 30)
-plt.tight_layout()
-plt.savefig('launch_2021_by_Launch_sites.png')
-#%% By Launch Vehicle
-
-#%% 
 fig=plt.figure(figsize=(12,8),dpi=300)
 axes1 = fig.add_axes([0.1, 0.1, 0.8, 0.8]) # main axes
 axes1.xaxis.set_ticks(np.arange(0,len(dict_sites)))
@@ -217,3 +193,57 @@ axes2.legend(patches,xaxis_labels,loc='center right',bbox_to_anchor=(1.1, 0.5),p
 for font in p_text:
     font.set_fontproperties(fprop)
 plt.savefig('launch_2021_by_sites2.png')
+
+#%% By Launch Vehicle
+dict_vehicles = {'Antares' 'Ariane-5' 'Atlas-V' 'CZ-2C' 'CZ-2C/YZ-1S' 'CZ-2D' 'CZ-2F'
+ 'CZ-3B/E' 'CZ-3C/E' 'CZ-4B' 'CZ-4C' 'CZ-5B' 'CZ-6' 'CZ-7' 'CZ-7A'
+ 'Delta-IVH' 'Electron' 'Epsilong' 'Falcon-9B5' 'Firefly-Alpha'
+ 'GSLV-MKII' 'H-IIA' 'Hyperbola-1' 'KSLV-II' 'Kuaizhou-1A' 'LauncherOne'
+ 'Minotar-1' 'PSLV-DL' 'Pegasus-XL' 'Proton-M' 'Rocket-3.3' 'Simorgh'
+ 'Soyuz-2.1a' 'Soyuz-2.1a/Fregat' 'Soyuz-2.1b' 'Soyuz-2.1b/Fregat'
+ 'Soyuz-2.1v/Volga' 'Vega'}
+cc_dict = {'CHN':'#A30000','ESA':'#194852','IND':'#3989b9','IRN':'cyan','JPN':'#fcc9b9','RUS':'#0033A0','SKO':'#FFA500','USA':'#002868'}
+vehicles_colors = []
+launch_country = np.array(launch_country)
+for vehicle in L_vehicles:
+    v_idx = launch_vehicles.index(vehicle)
+    v_country = launch_country[v_idx]
+    v_bar_color = cc_dict[v_country]
+    vehicles_colors.append(v_bar_color)
+    #print(site,s_country,s_bar_color)
+vehicles_colors = np.array(vehicles_colors)
+# Launch Vehicles
+launch_Byvehicles = np.array(launch_Byvehicles)
+L_vehicles = np.array(L_vehicles)
+lv_idx = argsort(launch_Byvehicles)
+fig4,ax4 = plt.subplots(1,figsize=(12,8),dpi=300)
+plt.bar(L_vehicles[lv_idx],launch_Byvehicles[lv_idx],color = vehicles_colors[lv_idx])
+for rect in ax4.patches:
+    y_value = rect.get_height()
+    x_value = rect.get_x()+rect.get_width()/2
+    space = 0
+    va = 'bottom'
+    if y_value<0:
+        space*=-1
+        va='top'
+    label="{:.0f}".format(y_value)
+    ax4.annotate(
+        label,
+        (x_value,y_value),
+        xytext=(0,space),
+        textcoords = "offset points",
+        ha = 'center',
+        va = va
+    )  
+plt.setp(ax4.get_xticklabels(),rotation=45,ha="right",rotation_mode="anchor")
+ax4.yaxis.set_major_locator(MultipleLocator(5))
+ax4.yaxis.set_minor_locator(MultipleLocator(1))
+plt.title('2021年全球航天入轨按火箭统计',fontproperties = fprop_title, fontsize = 30)
+plt.ylabel('发射次数',fontproperties=fprop)
+plt.xlabel('运载火箭',fontproperties=fprop)
+from datetime import datetime
+time_now = datetime.now(pytz.timezone('Asia/Shanghai')).strftime('%Y/%m/%d %H:%M:%S')
+ax4.text(.9, 1.35,"截至北京时间："+ time_now, fontproperties=fprop,color="gray",transform=ax.transAxes,va='center')
+ax4.text(.9, 1.30,"绘制：@Vony7", fontproperties=fprop,color="gray", transform=ax.transAxes)
+plt.tight_layout()
+plt.savefig('launch_2021_by_lv.png')
